2508423c0cd476cf2f10f3084650ed7fc3843a6b
Enretro <envpogled@gmail.com>
2021-02-08 17:08:29 +0100

    Update cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,28 @@
 exports.handler = function(event, contex, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcom to the super secret area"
-  })
+  const secretContent = `
+  <h3>Welcome To Secret Area</h3>
+  <p>We can tell you that the sky is <srtong>blue</strong>, and en plus en is equals tree.</p>
+  `
+  
+  let body
+
+  if (event.body) {
+    body = JSON.parse(event.body)
+  } else {
+    body = {}  	
+  }
+
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    })
+
+  } else {
+    callback(null, {
+      statusCode: 401
+    })
+  } 
+
+  
 }
