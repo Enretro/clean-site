a62ab98af79b512e61e6c396fc109477bb8dbbdb
Enretro <envpogled@gmail.com>
2021-02-09 10:24:01 +0100

    Add React component to page

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,13 @@
+// importing main core react libary
+import React from 'react'
+
+function MyAmazingComponent() {
+  return (
+    <div>
+      <h1 className="section-title section-title--blue">This is En Amazing React Component</h1>
+      <p>React is great!!!</p>
+    </div>
+  )
+}
+
+export default MyAmazingComponent
