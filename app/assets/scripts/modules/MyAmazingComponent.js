4676c87b353fb710bcff8a069c94d04ec8dc96a4
Enretro <envpogled@gmail.com>
2021-02-11 18:09:35 +0100

    add grid to css

--- app/assets/scripts/modules/MyAmazingComponent.js
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -5,7 +5,7 @@ function MyAmazingComponent() {
   return (
     <div>
       <h1 className="section-title section-title--blue">This is En Amazing React Component</h1>
-      <p>React is great!!!</p>
+      <p></p>
     </div>
   )
 }
